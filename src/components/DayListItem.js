8c5e8497f8eef1d9edefd93c1c892e69573aad28
Silwan Aleryani <silwanaleryani@Silwans-iMac.local>
2020-09-28 22:28:14 -0400

    DayList and DayListItem

---
+++ src/components/DayListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+import classnames from "classnames"
+import "components/DayListItem.scss";
+
+function formatSpots(spots){
+  if (spots === 0) {
+    return "no spots remaining";
+  }
+  if (spots === 1 ){
+    return "1 spot remaining";
+  }
+  if (spots > 1){
+    return spots + " spots remaining";
+  }
+}
+
+export default function DayListItem(props) {
+  const liClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+  
+  return (
+    <li className ={liClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
