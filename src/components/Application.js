d81a4cb1686fa02009a4ac1724b1737c19778d0d
Silwan Aleryani <silwanaleryani@Silwans-iMac.local>
2020-10-03 14:09:25 -0400

    separation of concerns

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,112 +3,56 @@ import DayList from "components/DayList";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
+
+import useApplicationData from '../hooks/useApplicationData';
 const axios = require('axios');
 
-
 export default function Application(props) {
- 
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
-  const setDay = (day) => {
-    setState({...state , day})
-  }
 
-  const [state, setState] = useState (
-    {
-      day: 'Monday',
-      days: [],
-      appointments: {},
-      interviewers: {}
+  const appointments = getAppointmentsForDay(state, state.day).map(
+    appointment => {
+      return (
+        <Appointment
+          key={appointment.id}
+          {...appointment}
+          interview={getInterview(state, appointment.interview)}
+          interviewers={getInterviewersForDay(state, state.day)}
+          bookInterview={bookInterview}
+          cancelInterview={cancelInterview}
+        />
+      );
     }
   );
-
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    return axios.put(`/api/appointments/${id}`, appointment)
-      .then((response) => {
-        console.log(response)
-        setState(prev => ({ ...prev, appointments }));
-      });
-  }
-  
-  function cancelInterview(id) {
-    const appointment = { 
-      ...state.appointments[id], 
-      interview: null };
-      
-    const appointments = { 
-      ...state.appointments, 
-      [id]: appointment };
-
-    return axios.delete(`/api/appointments/${id}`, appointment)
-      .then(() => {
-        setState(prev => ({ ...prev, appointments }));
-      })
-  }
-
-  const dailyAppointments = getAppointmentsForDay(state,state.day)
-  const interviewersOfDay = getInterviewersForDay(state, state.day);
-
-  useEffect(()=>{
-    Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
-    ]).then(all => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }))
-    })
-      .catch(err => {
-        console.log(err)
-      })
-  }, []);
-  const schedule = dailyAppointments.map((appointment)=> 
-  <Appointment 
-    key={appointment.id} 
-    id= {appointment.id} 
-    time={appointment.time} 
-    interviewers= {interviewersOfDay} 
-    interview={getInterview(state,appointment.interview)} 
-    bookInterview={bookInterview}
-    cancelInterview = {cancelInterview}
-  />);
   return (
     <main className="layout">
       <section className="sidebar">
-        {
-        <>
-          <img
+        <img
           className="sidebar--centered"
           src="images/logo.png"
           alt="Interview Scheduler"
         />
-        <hr className="sidebar__separator sidebar--centered"
-        /> 
+        <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList
-            days={state.days}
-            day={state.day}
-            setDay={setDay}
-          />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
           alt="Lighthouse Labs"
         />
-        </>
-        }
       </section>
       <section className="schedule">
-        {schedule}
-        <Appointment key="last" time="5pm" />
+        <section className="schedule">
+          {appointments}
+          <Appointment key="last" time="5pm" />
+        </section>
       </section>
     </main>
   );
