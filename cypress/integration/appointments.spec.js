913b5161c20550cc8972b31e79edebcac7db5228
Silwan Aleryani <silwanaleryani@Silwans-iMac.local>
2020-10-07 22:13:32 -0400

    fix a bug

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -23,22 +23,26 @@ describe("Appointments", () => {
     cy.get('[data-testid="student-name-input"]')
       .clear()
       .type('Lydia Miller-Jones');
+
     cy.get('[alt="Tori Malcolm"]')
       .click();
     cy.contains("Save").click();
+
     cy.contains('Saving').should('exist');
+    
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Tori Malcolm");
   });
-  it('should cancel an interview', () => {
+  it("should cancel an interview", () => {
     cy.get("[alt=Delete]")
-      .first()
       .click({ force: true });
-    cy.contains('Confirm').click();
-    cy.contains('Deleteing').should('exist');
-    cy.contains('Deleteing').should('not.exist');
+  
+    cy.contains("Confirm").click();
+  
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+  
     cy.contains(".appointment__card--show", "Archie Cohen")
-    .should("not.exist");
-
+      .should("not.exist");
   });
 })
